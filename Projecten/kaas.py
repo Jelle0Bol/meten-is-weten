df048ca48b0f00f64d85778aa0c27f78e1a55959
Jelle0Bol <99067766@mydavinci.nl>
2021-09-08 10:49:54 +0200

    kaas

--- Projecten/kaas.py
+++ Projecten/kaas.py
@@ -2,4 +2,6 @@ getal = int(input('Wat is je getal?'))
 if getal > 300:
     getal = str(getal)
     print(getal + ' is het grootste getal')
-
+elif getal < 300:
+    getal = str(getal)
+    print(getal + ' is het kleinste getal')
